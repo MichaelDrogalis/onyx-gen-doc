64ac80e970993e6772485f95c739e7f047d71663
Colin Hicks <colin.hicks@gmail.com>
2016-10-01 02:14:50 -0400

    gen catalog-entry; maintain order

--- src/onyx/gen_doc.clj
+++ src/onyx/gen_doc.clj
@@ -25,7 +25,7 @@
     v))
 
 (defn format-edn [edn & [width]]
-  (binding [pprint/*print-right-margin* (or width 20)]
+  (binding [pprint/*print-right-margin* (or width 40)]
     (format-md :clojure edn)))
 
 (defn format-comment [message]
@@ -55,10 +55,6 @@
       (table/table-str (into [header] body)
                        :style :github-markdown))))
 
-(defn mapdown-parse [markdown]
-  ;; todo rethrow ex with custom message
-  (mapdown/parse markdown))
-
 (def barrier-re #":{10,200}")
 (def block-ticks-re #"(?m)```(?:\w*)")
 (def ticks-re #"`(.*?)`")
@@ -69,13 +65,23 @@
       (s/replace ticks-re "$1")))
 
 (defn catalog-entry [information-model key]
-  (get-in information-model [:catalog-entry key :model]))
+  (let [entry (get-in information-model [:catalog-entry key :model])]
+    (assert entry ":model keyword must specify a catalog-entry in the information model.")
+    entry))
 
 (defn lifecycle-entry [information-model key]
-  (get-in information-model [:lifecycle-entry key :model]))
+  (let [entry (get-in information-model [:lifecycle-entry key :model])]
+    (assert entry ":model keyword must specify a lifecycle-entry in the information model.")
+    entry))
 
-(defn display-order [information-model key]
-  (get-in information-model [:display-order key]))
+(def priority-display-order [:onyx/name :onyx/plugin :onyx/type :onyx/medium])
+
+(defn display-order-comparator [information-model key]
+  (let [model-order (get-in information-model [:display-order key])
+        ordering (zipmap (into priority-display-order model-order) (range))]
+    (fn [a b]
+      (compare (get ordering a (Math/abs (hash a)))
+               (get ordering b (Math/abs (hash b)))))))
 
 (defmulti gen-section (fn [config params]
                         (:display params)))
@@ -89,8 +95,9 @@
           (str (format-comment params) "\n" out-md)
           (or out-md "")))
       (catch Throwable ex
+        (when (:throw? config) (throw ex))
         (str "<!-- PARSE ERROR: " (.getMessage ex) "\n"
-             (with-out-str (pprint/pprint (ex-data ex)))
+             (with-out-str (pprint/pprint (or (ex-data ex) params)))
              "-->")))))
 
 (defmethod gen-section :default
@@ -98,18 +105,63 @@
   (throw (ex-info "Unhandled :display type" {:params params})))
 
 (defmethod gen-section :attribute-table
-  [config {:keys [model columns]}])
+  [{:keys [information-model presets]}
+   {:keys [model columns width] :or {width 800}}]
+  (let [entry (-> (display-order-comparator information-model model)
+                  (sorted-map-by)
+                  (into (catalog-entry information-model model)))
+        columns' (if (keyword? columns)
+                   (get presets columns)
+                   columns)
+        _ (assert columns' ":columns must be a vector of column specs or a keyword specifying a config preset.")]
+    (gen-table entry columns' width)))
+
+(defn infer-catalog-entry-values [catalog-entry merge-additions]
+  (reduce-kv
+   (fn [m k {:keys [default type optional? choices deprecation-version doc]}]
+     (when (or deprecation-version
+               (identical? :gen-doc-ignore (k merge-additions)))
+       m)
+     (let [default' (or default (first choices))]
+       (if default'
+         (assoc m k default')
+         (case type
+           :boolean (assoc m k false)
+           :string (when-not optional?
+                     (assoc m k doc))
+           (:long :map) (when-not optional?
+                          (assoc m k :gen-doc-please-handle-in-merge-additions))
+           :keyword (when-not optional?
+                      (assoc m k (keyword (str "my-" (name k)))))
+           m))))
+   {}
+   catalog-entry))
+
+(defmethod gen-section :catalog-entry
+  [{:keys [information-model]}
+   {:keys [model merge-additions width]}]
+  (let [entry (-> (catalog-entry information-model model)
+                  (infer-catalog-entry-values merge-additions)
+                  (merge merge-additions))
+        sorted (sorted-map-by (display-order-comparator information-model model))]
+    (format-edn (into sorted entry) width)))
+
+(defmethod gen-section :lifecycle-entry
+  [{:keys [information-model]}
+   {:keys [model width]}]
+  (let [entry (lifecycle-entry information-model model)]
+    (format-edn entry width)))
 
 (defmethod gen-section :header
   [{:keys [verbose? information-model]}
    {:keys [valid-structure? all-params]}]
-  (assert valid-structure? "Template sections must be delineated by :::::::::: at top and bottom")
+  (assert valid-structure? "Template sections must be delineated by :::::::::: at top and bottom.")
   (when verbose?
     ;; todo: set difference of the info-model catalog/lifecycle keys vs. markdown sections
     ))
 
 (defn parse-section [config md]
-  (->> (mapdown-parse md)
+  (->> (mapdown/parse md)
        (reduce-kv (fn [m k v]
                     (->> v
                          (strip-format)
@@ -136,24 +188,34 @@
 
 (comment
   (gen-template
-   {}
+   {:throw? true
+    :information-model
+    {:catalog-entry
+     {:aplugin/read
+      {:model {:foo {:doc "foo" :type :string :default "bar"}
+               :baz {:doc "" :type :map }}}}
+     :display-order
+     {:aplugin/read
+      [:baz :foo]}}}
    (str
     "# Header ... \n"
     "sit amet ... \n\n"
     ":::::::::::::::::::: \n"
-    ":display `:attribute-table` \n"
-    ":model :onyx.plugin.kafka/read-messages \n"
+    ":display `:attribute-table` \n\n"
+    ":model :aplugin/read \n"
     ":columns [[:key \"Parameter\"] [:type \"Type\"]] \n"
     ":::::::::::::::::::: \n\n"
     "## Intermezzo ... \n"
     "lorem ipsum \n\n"
     ":::::::::::::::::::: \n"
     ":display :catalog-entry \n"
-    ":model :onyx.plugin.kafka/read-messages \n"
-    ":defaults \n
+    ":model :aplugin/read \n"
+    ":merge-additions \n
 ```clojure\n
-{:foo \"bar\" \n
- :baz :quux}\n
+{:onyx/name :read \n
+ :foo \"buz\" \n
+ :extra true \n
+ :onyx/secondary-key 100}\n
 ```\n"
     ":::::::::::::::::::: \n\n"
     "## Footer ..."
--- test/onyx/gen_doc_test.clj
+++ test/onyx/gen_doc_test.clj
@@ -3,6 +3,7 @@
             [onyx.gen-doc :as gen-doc]
             [onyx.kafka.information-model :as kafka]))
 
+
 (deftest table
   (let [body (gen-doc/table-body
               (get-in kafka/model [:catalog-entry :onyx.plugin.kafka/read-messages :model])
@@ -13,3 +14,9 @@
                [:doc "Description"]])]
     (is (= ["`:kafka/chan-capacity`" "`:long`" "`true`" "`1000`" "The buffer size of the Kafka reading channel."]
            (vec (last body))))))
+
+
+(deftest display-order-comparator
+  (let [cx (gen-doc/display-order-comparator kafka/model :onyx.plugin.kafka/read-messages)]
+    (is (= [:onyx/name :kafka/topic :kafka/partition :onyx/batch-size]
+           (sort cx [:kafka/partition :kafka/topic :onyx/name :onyx/batch-size])))))
