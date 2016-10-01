182d6b2e413a76edff91bb42bb62b6ada03ec721
Colin Hicks <colin.hicks@gmail.com>
2016-09-30 23:09:02 -0400

    supports a debug header

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-doc-gen "0.9.12-SNAPSHOT"
+(defproject org.onyxplatform/onyx-gen-doc "0.9.12-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [table "0.5.0"]
                  [mapdown "0.2.1"]]
